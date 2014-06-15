b32d26db9c70d5c45bd4f79b3c26576de1c30c2e
Shlomi Matichin <shlomi@stratoscale.com>
2014-06-15 19:03:00 +0300

    Installs as a python egg

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="solvent",
+    version="1.0",
+    author="Shlomo Matichin",
+    author_email="shlomomatichin@gmail.com",
+    description=(
+        "Manage official build product flow, using osmosis as a transport"),
+    keywords="git repos repositories scm buildproducts build products",
+    url="http://packages.python.org/solvent",
+    packages=['solvent'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+    ],
+)
