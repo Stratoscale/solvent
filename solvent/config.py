f1cabc889dd93e26295501097ac9cbf90890a1cd
Shlomi Matichin <shlomi@stratoscale.com>
2014-07-24 13:34:56 +0300

    Bugfix: Select local osmosis depends if user is root, to avoid permission denied on /var/lib/osmosis

--- solvent/config.py
+++ solvent/config.py
@@ -1,7 +1,9 @@
 import yaml
 import os
 
-LOCAL_OSMOSIS = 'localhost:1010'
+LOCAL_OSMOSIS_IF_ROOT = 'localhost:1010'
+LOCAL_OSMOSIS_IF_NOT_ROOT = 'localhost:1010'
+LOCAL_OSMOSIS = None
 OFFICIAL_OSMOSIS = None
 OFFICIAL_BUILD = False
 WITH_OFFICIAL_OBJECT_STORE = True
@@ -22,6 +24,12 @@ def load(filename):
         CLEAN = True
     if WITH_OFFICIAL_OBJECT_STORE and OFFICIAL_OSMOSIS is None:
         raise Exception("Configuration file must contain 'OFFICIAL_OSMOSIS' field")
+    global LOCAL_OSMOSIS
+    if LOCAL_OSMOSIS is None:
+        if os.getuid() == 0:
+            LOCAL_OSMOSIS = LOCAL_OSMOSIS_IF_ROOT
+        else:
+            LOCAL_OSMOSIS = LOCAL_OSMOSIS_IF_NOT_ROOT
 
 
 def objectStoresOsmosisParameter():
