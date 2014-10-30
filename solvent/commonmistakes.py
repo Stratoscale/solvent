febcab04d27ba09e9a9b836895251dc5f47b1872
Shlomi Matichin <shlomi@stratoscale.com>
2014-10-30 17:01:05 +0200

    made mount checking public

--- solvent/commonmistakes.py
+++ solvent/commonmistakes.py
@@ -4,7 +4,7 @@ import os
 
 class CommonMistakes:
     def checkDirectoryBeforeSubmission(self, dir):
-        under = self._mountedUnder(dir)
+        under = self.mountedUnder(dir)
         if len(under) > 0:
             message = "there are active mount points mounted under '%s':\n%s" % (
                 dir, "\n".join([m['path'] for m in under]))
@@ -12,7 +12,8 @@ class CommonMistakes:
                 "Common mistakes protections found the following. Please correct "
                 "it, or pass the --noCommonMistakesProtection.\n" + message)
 
-    def _mount(self):
+    @classmethod
+    def _mount(cls):
         output = run.run(["mount"])
         result = []
         for line in output.strip().split("\n"):
@@ -21,6 +22,7 @@ class CommonMistakes:
             result.append(dict(type=fields[4], path=fields[2]))
         return result
 
-    def _mountedUnder(self, dir):
+    @classmethod
+    def mountedUnder(cls, dir):
         realPath = os.path.realpath(dir)
-        return [m for m in self._mount() if m['path'].startswith(realPath)]
+        return [m for m in cls._mount() if m['path'].startswith(realPath)]
