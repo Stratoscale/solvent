37baf4b9929b7d894cdb090bb4874d7a782a2c38
Eliran Levi <eliran@stratoscale.com>
2017-08-30 13:35:57 +0300

    Fix env var for osmosis when locale name is invalid
    See https://github.com/ros-drivers/hokuyo_node/issues/3

--- solvent/run.py
+++ solvent/run.py
@@ -1,12 +1,19 @@
 import subprocess
 import logging
+import os
+
+
+def fix_env_in_case_of_invalid_locale_name(env):
+    """See https://github.com/ros-drivers/hokuyo_node/issues/3"""
+    env["LC_ALL"] = "C"
 
 
 def run(command, cwd=None):
+    env = os.environ.copy()
+    fix_env_in_case_of_invalid_locale_name(env)
     try:
-        return subprocess.check_output(
-            command, cwd=cwd, stderr=subprocess.STDOUT,
-            stdin=open("/dev/null"), close_fds=True)
+        return subprocess.check_output(command, cwd=cwd, stderr=subprocess.STDOUT, stdin=open("/dev/null"),
+                                       close_fds=True, env=env)
     except subprocess.CalledProcessError as e:
         logging.error("Failed command '%s' output:\n%s" % (command, e.output))
         raise
